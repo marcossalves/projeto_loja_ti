0d0c4845446cb73218bde2bc14bdedfdddb90703
marcossalves <marcossa.ti@terra.com.br>
2020-08-02 17:15:28 -0300

    alterar foto

---
+++ service/usuario/alterarfoto.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+Vamos construir os cabeçalhos para trabalho com a api
+*/
+header("Access-Control-Allow-Origin:*");
+header("Content-Type:application/json;charset=utf-8");
+
+/*Para efetuar o cadastro de dados no banco é preciso
+informar a api que essa ação irá ocorrer com o metodo PUT, que 
+e responsável pela atualização de dados da api.
+*/
+header("Access-Control-Allow-Methods:PUT");
+
+include_once "../../config/database.php";
+
+include_once "../../domain/usuario.php";
+
+$database = new Database();
+$db = $database->getConnection();
+
+$usuario = new Usuario($db);
+
+/*
+O cliente irá enviar os dado no formato Json. Porém
+ nós precisamos dos dados no formato php para cadstra em
+ banco de dados. 
+ Para realizar essa conversão iremos usar o comando json_decode
+ Assi o cliente envia os dados e estes serão convertidos para php
+*/
+$data = json_decode(file_get_contents("php://input"));
+
+#Verificar se os dados vindos do usuário estão preenchidos
+if(!empty($data->idusuario) && !empty($data->foto)){
+
+    
+    $usuario->foto = $data->foto;
+    $usuario->idusuario=$data->idusuario;
+
+    if($usuario->alterarfoto()){
+        header("HTTP/1.0 201");
+        echo json_encode(array("mensagem"=>"Foto alterada com sucesso!"));
+    }
+    else{
+        header("HTTP/1.0 400");
+        echo json_encode(array("mensagem"=>"Não foi possível alterar a foto"));
+    }
+}
+else{
+    header("HTTP/1.0 400");
+    echo json_encode(array("mensagem"=>"Você precisa preencher todos os campos"));
+}
+
+?>
